40e7a3ffc89de3f80f4819d0f05f9fe9a78a585e
Lopez Gonzalo <lopezgonzalo.sm89@gmail.com>
2021-12-01 21:47:45 -0300

    chore: design menu card

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./state/store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "./screens/Home";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <SafeAreaProvider>
+        <Home />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -5,26 +5,27 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image,
 } from "react-native";
+
+import tw from "tailwind-react-native-classnames";
+
+import uberX from "../assets/UberX.webp";
+import food from "../assets/food.png";
+import { Icon } from "react-native-elements";
 
 const data = [
   {
     id: "123",
     title: "Get a ride",
-    image: "",
+    image: uberX,
     screen: "MapScreen",
   },
   {
-    id: "123",
-    title: "Get a ride",
-    image: "",
-    screen: "MapScreen",
-  },
-  {
-    id: "123",
-    title: "Get a ride",
-    image: "",
-    screen: "MapScreen",
+    id: "456",
+    title: "Order food",
+    image: food,
+    screen: "EatsScreen",
   },
 ];
 
@@ -32,10 +33,27 @@ const NavOptions = () => {
   return (
     <FlatList
       data={data}
+      horizontal
       keyExtractor={(item) => item.id}
-      renderItem={({ item: { title } }) => (
-        <TouchableOpacity>
-          <Text>{title}</Text>
+      renderItem={({ item: { title, image } }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{
+                width: 120,
+                height: 120,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+          </View>
+          <Text style={tw`mt-2 text-lg font-semibold`}>{title}</Text>
+          <Icon
+            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            name="arrowright"
+            color="white"
+            type="antdesign"
+          />
         </TouchableOpacity>
       )}
     />
